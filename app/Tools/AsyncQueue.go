f0d0872766416e32fe51ac9088acd713e2fd7a3d
linjifu <613555424@qq.com>
2023-03-29 14:58:59 +0800

    1

--- app/Tools/AsyncQueue.go
+++ app/Tools/AsyncQueue.go
@@ -2,8 +2,10 @@ package Tools
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/gomodule/redigo/redis"
 	"goFrame/app/Jobs"
+	"time"
 )
 
 type Job interface {
@@ -90,14 +92,55 @@ func (q *AsyncQueue) Pop() (bool, error) {
 	conn := redisPool.Get()
 	defer conn.Close()
 
-	data, err := redis.String(conn.Do("LPOP", q.name))
+	//阻塞10秒，防止连接超时
+	fmt.Println("["+q.name+"]", time.Now().Hour(), ":", time.Now().Minute(), ":", time.Now().Second())
+	data, err := redis.Strings(conn.Do("BLPOP", q.name, 10))
 	if err != nil {
 		return false, err
 	}
 
-	job, err := q.deserialize(data)
+	job, err := q.deserialize(data[1])
 	if err != nil {
 		return false, err
 	}
+
 	return job.Run()
+}
+
+func QueueStart() {
+	go func() {
+		queueListRedisKey := "queueList"
+		queueList := make(map[string]int8, 0)
+		for {
+			//获取连接池
+			redisPool, _ := GetRedisPool()
+			//获取连接
+			c := redisPool.Get()
+
+			tempQueueList, getErr := redis.Strings(c.Do("SMEMBERS", queueListRedisKey))
+			if getErr == nil {
+				for _, queueName := range tempQueueList {
+					if _, ok := queueList[queueName]; !ok {
+						//启动队列
+						go func(qName string) {
+							AsyncQueue := NewAsyncQueue(qName)
+							for {
+								AsyncQueue.Pop()
+							}
+						}(queueName)
+						//放入已启动列表
+						queueList[queueName] = 1
+						fmt.Println("队列【" + queueName + "】已启动监听")
+					}
+				}
+			}
+
+			//关闭连接
+			c.Close()
+			//延时30分钟执行一次
+			time.Sleep(30 * time.Minute)
+		}
+	}()
+	fmt.Println("队列模块启动成功")
+	select {}
 }
--- cmd/Queue/Queue.go
+++ cmd/Queue/Queue.go
@@ -1,11 +1,8 @@
 package Queue
 
 import (
-	"fmt"
-	"github.com/gomodule/redigo/redis"
 	"github.com/spf13/cobra"
 	"goFrame/app/Tools"
-	"time"
 )
 
 type Queue struct {
@@ -27,41 +24,42 @@ func (q Queue) Cmd() *cobra.Command {
 }
 
 func (q *Queue) run() {
-	go func() {
-		queueListRedisKey := "queueList"
-		queueList := make(map[string]int8, 0)
-		for {
-			//获取连接池
-			redisPool, _ := Tools.GetRedisPool()
-			//获取连接
-			c := redisPool.Get()
-
-			tempQueueList, getErr := redis.Strings(c.Do("SMEMBERS", queueListRedisKey))
-			if getErr == nil {
-				for _, queueName := range tempQueueList {
-					if _, ok := queueList[queueName]; !ok {
-						//启动队列
-						go func(qName string) {
-							AsyncQueue := Tools.NewAsyncQueue(qName)
-							for {
-								AsyncQueue.Pop()
-								//延时1秒钟执行一次
-								time.Sleep(1 * time.Second)
-							}
-						}(queueName)
-						//放入已启动列表
-						queueList[queueName] = 1
-						fmt.Println("队列【" + queueName + "】已启动监听")
-					}
-				}
-			}
-
-			//关闭连接
-			c.Close()
-			//延时三分钟执行一次
-			time.Sleep(30 * time.Minute)
-		}
-	}()
-	fmt.Println("队列模块启动成功")
-	select {}
+	Tools.QueueStart()
+	//go func() {
+	//	queueListRedisKey := "queueList"
+	//	queueList := make(map[string]int8, 0)
+	//	for {
+	//		//获取连接池
+	//		redisPool, _ := Tools.GetRedisPool()
+	//		//获取连接
+	//		c := redisPool.Get()
+	//
+	//		tempQueueList, getErr := redis.Strings(c.Do("SMEMBERS", queueListRedisKey))
+	//		if getErr == nil {
+	//			for _, queueName := range tempQueueList {
+	//				if _, ok := queueList[queueName]; !ok {
+	//					//启动队列
+	//					go func(qName string) {
+	//						AsyncQueue := Tools.NewAsyncQueue(qName)
+	//						for {
+	//							AsyncQueue.Pop()
+	//							//延时1秒钟执行一次
+	//							time.Sleep(1 * time.Second)
+	//						}
+	//					}(queueName)
+	//					//放入已启动列表
+	//					queueList[queueName] = 1
+	//					fmt.Println("队列【" + queueName + "】已启动监听")
+	//				}
+	//			}
+	//		}
+	//
+	//		//关闭连接
+	//		c.Close()
+	//		//延时30分钟执行一次
+	//		time.Sleep(30 * time.Minute)
+	//	}
+	//}()
+	//fmt.Println("队列模块启动成功")
+	//select {}
 }
